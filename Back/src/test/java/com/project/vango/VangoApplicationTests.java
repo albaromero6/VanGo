b5b7dfc66daae3e7bdfc93ddb4f38c47ce01ece9
Alba Romero <albaromeroa6@gmail.com>
2025-04-25 14:52:04 +0200

    Subida Back Inicial

---
+++ Back/src/test/java/com/project/vango/VangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
