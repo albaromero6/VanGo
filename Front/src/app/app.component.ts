674c25c0309ef994776cb87250af4b8fbc5dcaad
Alba Romero <albaromeroa6@gmail.com>
2025-05-16 17:56:53 +0200

    SCSS básico y componente navbar Ok

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Front';
+  title = 'VanGo';
 }
