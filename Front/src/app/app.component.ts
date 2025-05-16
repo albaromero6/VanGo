57fc4a556aab47d57aed2cd819cb3713351083fd
Alba Romero <albaromeroa6@gmail.com>
2025-05-16 19:39:53 +0200

    Sección Types Ok

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from './components/footer/footer.component';
+import { TypesComponent } from './sections/types/types.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent ],
+  imports: [ NavbarComponent, TypesComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'VanGo';
 }
