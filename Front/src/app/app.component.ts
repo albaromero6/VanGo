761db1ce3f29f5e6c4d4d0a47f28c2f359c8687f
Alba Romero <albaromeroa6@gmail.com>
2025-05-18 17:04:44 +0200

    Prueba

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { TypesComponent } from './sections/types/types.component';
 import { StepsComponent } from './sections/steps/steps.component';
+import { ApplicationComponent } from './sections/application/application.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavbarComponent, TypesComponent, StepsComponent, FooterComponent ],
+  imports: [ NavbarComponent, TypesComponent, StepsComponent, ApplicationComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
