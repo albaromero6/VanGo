7366c4ed1876e0f9df5de9fa771b058c9e9bd513
Alba Romero <albaromeroa6@gmail.com>
2025-05-18 16:48:07 +0200

    Sección Steps Ok

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { TypesComponent } from './sections/types/types.component';
+import { StepsComponent } from './sections/steps/steps.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavbarComponent, TypesComponent, FooterComponent ],
+  imports: [ NavbarComponent, TypesComponent, StepsComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
