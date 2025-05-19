3681c6bdb14424cf8d42207147e84e9d65200ab4
Alba Romero <albaromeroa6@gmail.com>
2025-05-19 20:22:52 +0200

    Enlaces Footer Ok

--- Front/src/app/components/footer/footer.component.ts
+++ Front/src/app/components/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CatalogComponent } from '../../pages/catalog/catalog.component';
+import { SelectusComponent } from '../../pages/selectus/selectus.component';
+import { RoutesComponent } from '../../pages/routes/routes.component';
+import { ContactComponent } from '../../pages/contact/contact.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CatalogComponent, SelectusComponent, RoutesComponent, ContactComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
