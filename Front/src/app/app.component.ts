e6168b3296f0c8442bac2b29f2b3072d58043681
Alba Romero <albaromeroa6@gmail.com>
2025-05-19 13:17:34 +0200

    Sección Hero Ok

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { TypesComponent } from './sections/types/types.component';
 import { StepsComponent } from './sections/steps/steps.component';
 import { ApplicationComponent } from './sections/application/application.component';
+import { HeroComponent } from './sections/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavbarComponent, TypesComponent, StepsComponent, ApplicationComponent, FooterComponent ],
+  imports: [ NavbarComponent, HeroComponent, TypesComponent, StepsComponent, ApplicationComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
