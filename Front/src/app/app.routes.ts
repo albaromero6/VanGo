77a8d3d6b893349ea47b4437773f61fed69b70d0
Alba Romero <albaromeroa6@gmail.com>
2025-05-20 16:29:05 +0200

    Ubicación Ok

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { SelectusComponent } from './pages/selectus/selectus.component';
 import { RoutesComponent } from './pages/routes/routes.component';
@@ -7,15 +6,17 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { LocationComponent } from './pages/location/location.component';
 
 export const routes: Routes = [
 
-    { path: '', component: HomeComponent },
-    { path: 'catalogo', component: CatalogComponent },
-    { path: 'eligenos', component: SelectusComponent },
-    { path: 'rutas', component: RoutesComponent },
-    { path: 'contacto', component: ContactComponent },
-    { path: 'registro', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
+    { path: '',              component: HomeComponent },
+    { path: 'catalogo',      component: CatalogComponent },
+    { path: 'eligenos',      component: SelectusComponent },
+    { path: 'rutas',         component: RoutesComponent },
+    { path: 'contacto',      component: ContactComponent },
+    { path: 'ubicacion',     component: LocationComponent },
+    { path: 'registro',      component: RegisterComponent },
+    { path: 'login',         component: LoginComponent },
 
 ];
