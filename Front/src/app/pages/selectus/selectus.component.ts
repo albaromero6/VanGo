0ff7df8624e7f31ad9f20f2e949d155f1af3f3c3
Alba Romero <albaromeroa6@gmail.com>
2025-06-07 12:55:46 +0200

    Añadido efecto reveal con animación en SelectUs Ok

--- Front/src/app/pages/selectus/selectus.component.ts
+++ Front/src/app/pages/selectus/selectus.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-selectus',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './selectus.component.html',
   styleUrl: './selectus.component.scss'
 })
-export class SelectusComponent {
+export class SelectusComponent implements OnInit {
+  private animatedElements: HTMLElement[] = [];
 
+  ngOnInit() {
+    // Inicializar los elementos animables
+    this.animatedElements = Array.from(document.querySelectorAll('.reveal-left, .reveal-top, .reveal-item')) as HTMLElement[];
+    // Verificar elementos visibles al cargar la página
+    this.checkVisibility();
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.checkVisibility();
+  }
+
+  private checkVisibility() {
+    const triggerBottom = window.innerHeight * 0.8;
+
+    this.animatedElements.forEach(element => {
+      const elementTop = element.getBoundingClientRect().top;
+
+      if (elementTop < triggerBottom) {
+        element.classList.add('active');
+      }
+    });
+  }
 }
