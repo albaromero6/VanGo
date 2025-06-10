4fbe0db6fa502ae48a00cfd3091bb7ef733f18f7
Alba Romero <albaromeroa6@gmail.com>
2025-06-10 17:47:48 +0200

    Home Controller

---
+++ Back/src/main/java/com/project/vango/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.project.vango.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "API Vango funcionando!";
+    }
+}
