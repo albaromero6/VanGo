fc641eee4b8928f938b40623938de7a1798ccc81
Alba Romero <albaromeroa6@gmail.com>
2025-06-10 17:40:41 +0200

    Añadido printl

--- Back/src/main/java/com/project/vango/config/SecurityConfig.java
+++ Back/src/main/java/com/project/vango/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
+        System.out.println("SecurityConfig cargado");
     }
 
     @Bean
